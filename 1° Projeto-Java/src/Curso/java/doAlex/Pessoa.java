5d751f6b67e183ae21c6f84a53aff87d2441d1ec
Usuário <Usurio@DESKTOP-9QEOTCC>
2024-01-23 13:22:53 -0200

    Adicionando o Campo idade na classe pessoa

--- 1° Projeto-Java/src/Curso/java/doAlex/Pessoa.java
+++ 1° Projeto-Java/src/Curso/java/doAlex/Pessoa.java
@@ -6,6 +6,16 @@ public class Pessoa {
 	private String nome;
 	
 	private String Sobrenome;
+	
+	private int Idade;
+
+	public int getIdade() {
+		return Idade;
+	}
+
+	public void setIdade(int idade) {
+		Idade = idade;
+	}
 
 	public String getNome() {
 		return nome;
