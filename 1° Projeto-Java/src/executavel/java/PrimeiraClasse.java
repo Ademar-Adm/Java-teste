220e8b662bae9f6d360f39f44b563e5d1822ba4d
Usuário <Usurio@DESKTOP-9QEOTCC>
2024-01-23 12:07:31 -0200

    outra alteração

--- 1° Projeto-Java/src/executavel/java/PrimeiraClasse.java
+++ 1° Projeto-Java/src/executavel/java/PrimeiraClasse.java
@@ -3,7 +3,8 @@ package executavel.java;
 public class PrimeiraClasse {
 	public static void main(String[] args) {
 		System.out.println("1° Projeto no GitHub");
-		System.out.println("1° Alteração Projeto no GitHub");
-		
+		System.out.println("1° Alteração Projeto no GitHub");		
+		/*Teste*/
+		System.out.println("Outra alteração");
 	}
 }
