55fcae988112a369668d3054e7d9b0913151f16c
Usuário <Usurio@DESKTOP-9QEOTCC>
2024-01-24 22:42:27 -0200

    Segundo projeto em outra pasta

---
+++ 2° Projeto java/src/curso/java/SegundoProjetin.java
@@ -0,0 +1,7 @@
+package curso.java;
+
+public class SegundoProjetin {
+	public static void main(String[] args) {
+		System.out.println("Chip chip chapa chapa dubi dubi daba daba magic bum bum bum");
+	}
+}
